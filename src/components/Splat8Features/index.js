81fd21cfa7cbaf1e289a4958d202589adf5177df
wingkin <greenwinds@gmail.com>
2022-08-16 11:49:15 +0900

    主页字体样式的调整

--- src/components/Splat8Features/index.js
+++ src/components/Splat8Features/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import styles from './styles.module.css';
+// import styles from './styles.module.css';
 import Layout from '@theme/Layout';
 import styles1 from './style.css';
 // import styles2 from './css/bootstrap.min.css';
@@ -31,8 +31,26 @@ export default function Splat8Features() {
                      <p>斯普拉遁（日语：スプラトゥーン，英语：Splatoon，中国大陆旧译“喷射战士”、“喷色卡通”，台湾旧译“漆弹大作战”）是由任天堂开发并发行的第三人称射击游戏系列。第一款游戏《Splatoon》于2015年5月在Wii U平台发行。续作《Splatoon2》于2017年7月在任天堂Switch平台发行；其新增下载内容《Octo Expansion》于2018年6月发行。最新作《斯普拉遁3》预计于2022年9月9日在任天堂Switch平台发行，是系列首次中文化的作品。 - 维基百科</p>
                   </div>
                </div>
+               
+               <div class="row">
+    <div class="col col--3">
+      <div class="col-demo">3</div>
+    </div>
+    <div class="col col--3">
+      <div class="col-demo">3</div>
+    </div>
+    <div class="col col--3">
+      <div class="col-demo">3</div>
+    </div>
+    <div class="col col--3">
+      <div class="col-demo">3</div>
+    </div>
+  </div>
+  
             </div>
+
          </div>
+
          <a class="bloc-button btn btn-d scrollToTop" onclick="scrollToTarget('1',this)">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32">
                <path class="scroll-to-top-btn-icon" d="M30,22.656l-14-13-14,13"/>
